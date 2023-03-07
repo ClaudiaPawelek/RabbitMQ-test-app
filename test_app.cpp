95c64e3ae09856d96b334384d578cb13d02eecff
Klaudia Pawelek <klaudia.pawelek@gmail.com>
2023-03-07 18:18:32 +0000

    Use event loop instead of threads (transmitter)
    Receiver's code has not been updated for now, because it is just a simple app for testing and I am focused on transmitter's errors.

--- test_app.cpp
+++ test_app.cpp
@@ -101,6 +101,11 @@ public:
 		return evhandler_.isConnectionReady();
 	}
 
+    event_base* getEventBase()
+    {
+        return evbase_.get();
+    }
+
 private:
 
 	EventBasePtrT evbase_;
@@ -108,89 +113,152 @@ private:
 };
 
 
-class RabbitMQTransmitter 
+
+class RabbitMQTransmitter
 {
 public:
-  RabbitMQTransmitter(const std::string& exchangeName, const std::string& queueName)
-      : exchangeName_(exchangeName), queueName_(queueName) 
-      
+    RabbitMQTransmitter(const std::string& exchangeName, const std::string& queueName)
+        : exchangeName_(exchangeName), queueName_(queueName)
     {
-        processingThread = std::thread(&RabbitMQTransmitter::Start, this);
+        // Initialize libevent - not sure if this is needed!!!
+        // event_config* ev_config = event_config_new();
+        // event_config_set_flag(ev_config, EVENT_BASE_FLAG_EPOLL_USE_CHANGELIST);
+        // evbase_ = event_base_new_with_config(ev_config);
+        // event_config_free(ev_config);
+        
+        event_loop_thread_ = std::thread(&RabbitMQTransmitter::EventLoop, this);
+    }
+    ~RabbitMQTransmitter()
+    {
+        Stop();
     }
 
-  void Start() 
-  {
-    while(true)
-    {
-        try 
+    void EventLoop() //processing
+    {       
+        while(true)
         {
-        connectionHandler_ = std::make_shared<ConnHandler>();
-
-        connection_ = std::make_shared<AMQP::TcpConnection>(*connectionHandler_.get(), AMQP::Address("amqp://localhost/"));
-
-        channel_ = std::make_shared<AMQP::TcpChannel>(connection_.get());
-
-        channel_->declareExchange(exchangeName_, AMQP::fanout);
-
-        channel_->declareQueue(queueName_, AMQP::durable)
-            .onSuccess([this](const std::string& name, uint32_t, uint32_t) {
-                queueName_ = name;
-                channel_->bindQueue(exchangeName_, queueName_, "");
-                connectionHandler_->setConnectionReady(true);
-            });
-
-        connectionHandler_->Start();
-
-                // Tidy up after we've finished
-            if (channel_)
-            {
-                channel_.reset();
-            }
-            if (connection_)
-            {
-                connection_->close();
-                connection_.reset();
-            }
-            if (connectionHandler_)
-            {
-                connectionHandler_->Stop();
-                connectionHandler_.reset();
-            }	
-        } 
-        catch (const std::exception& e) 
-        {
-        std::cerr << "Failed to start RabbitMQ transmitter: " << e.what() << std::endl;
+            Start();
+            
+            // Start event loop
+            conn_handler_->Start();
+          
+            // Clean up
+            //event_base_dispatch(evbase_);
+            if (message_event_)
+                event_del(message_event_);
+            channel_.reset();
+            connection_.reset();
         }
     }
-   
-  }
 
-  void SendMessage(const std::string& messageBody) 
-  {
-    try 
+    void Start()
     {
-        AMQP::Envelope envelope(messageBody.c_str(), messageBody.size());
-        if (connectionHandler_ && channel_ && connectionHandler_->isConnectionReady())
-            channel_->publish(exchangeName_, "", envelope);
-    } 
-    catch (const std::exception& e) 
-    {
-      std::cerr << "Failed to send message: " << e.what() << std::endl;
+        conn_handler_ = std::make_shared<ConnHandler>();
+
+        // Create a new connection and channel
+        connection_ = std::make_shared<AMQP::TcpConnection>(*conn_handler_.get(), AMQP::Address("amqp://localhost/"));
+        
+        channel_ = std::make_shared<AMQP::TcpChannel>(connection_.get());
+        
+        channel_->onError([this](const char *message) {
+            std::cout<<"(RMQ tx) Channel error: " << message;
+            // Stop the handler
+            conn_handler_->Stop();
+
+        });
+
+        // Declare the exchange and queue
+        channel_->declareExchange(exchangeName_, AMQP::fanout)
+                .onSuccess([this]() {
+                    std::cout<<"(RMQ tx) Exchange '" << exchangeName_ << "' declared successfully.";
+                    }
+                )		
+                .onError([this](const char *message) {
+                    std::cout<<"(RMQ tx) Exchange '" << exchangeName_ << "' already exists. " << message;
+                    }
+                );
+
+        channel_->declareQueue(queueName_, AMQP::durable)
+                .onSuccess([this](const std::string& name, uint32_t, uint32_t) {
+                queueName_ = name;
+            
+                channel_->bindQueue(exchangeName_, queueName_, "")
+                        .onSuccess([this]() {
+                            std::cout<<"(RMQ tx) Successfully bound queue to exchange.";
+                            conn_handler_->setConnectionReady(true);
+                            // Set up the message sending event
+                            message_event_ =
+                            event_new(conn_handler_->getEventBase(), -1, EV_PERSIST, &RabbitMQTransmitter::OnSendMessage, this);
+            })
+                .onError([](const char *message) {
+                std::cout<<"(RMQ tx) Error binding queue to exchange: " << message;
+            });
+                
+        });
     }
-  }
 
-  
+    void Stop()
+    {
+        // Disable the message sending event
+        event_del(message_event_);
+        // Stop the connection handler and event loop
+        conn_handler_->Stop();
+        if (event_loop_thread_.joinable())
+        {
+            event_loop_thread_.join();
+        }
+    }
+    void SendMessage(const std::string& messageBody)
+    {
+        // Add the message to the message queue
+        message_queue_.push(messageBody);
+        if (message_event_ && conn_handler_->isConnectionReady())
+        {
+            // Activate the message sending event
+            event_active(message_event_, EV_WRITE, 0);
+        }
+    }
 
-  std::string exchangeName_;
-  std::string queueName_;
-  std::shared_ptr<AMQP::TcpConnection> connection_;
-  std::shared_ptr<AMQP::TcpChannel> channel_;
-  std::shared_ptr<ConnHandler> connectionHandler_;
+private:
+    static void OnSendMessage(evutil_socket_t fd, short events, void* arg)
+    {
+        auto self = reinterpret_cast<RabbitMQTransmitter*>(arg);
+        while (!self->message_queue_.empty())
+        {
+            const std::string& messageBody = self->message_queue_.front();
+            try
+            {
+                AMQP::Envelope envelope(messageBody.c_str(), messageBody.size());
+                if (self->conn_handler_->isConnectionReady())
+                {
+                    std::cout<<"publish!";
+                    self->channel_->publish(self->exchangeName_, "", envelope);
+                }
+            }
+            catch (const std::exception& e)
+            {
+                std::cerr << "Failed to send message: " << e.what() << std::endl;
+            }
+            self->message_queue_.pop();
+        }
+    }
 
-  std::thread processingThread;
+    std::string exchangeName_;
+    std::string queueName_;
+    std::shared_ptr<AMQP::TcpConnection> connection_;
+    std::shared_ptr<AMQP::TcpChannel> channel_;
+    std::shared_ptr<ConnHandler> conn_handler_;
+    event* message_event_ = nullptr; //raw pointer for now
+    std::queue<std::string> message_queue_;
+    std::thread event_loop_thread_;
 };
 
-class RabbitMQReceiver {
+
+// We don't care about the receiver for now
+// It has thread, but works fine, so for testing - let's leave it as it is
+// This task is all about transmitter.
+class RabbitMQReceiver 
+{
 public:
   RabbitMQReceiver(const std::string& exchangeName, const std::string& queueName, const std::string& routingKey)
       : exchangeName_(exchangeName), queueName_(queueName), routingKey_(routingKey) 
@@ -200,115 +268,110 @@ public:
 
       }
 
-  void Start() 
-  {
-    while(true)
-    {    
-        try 
-        {
-        connectionHandler_ = std::make_shared<ConnHandler>();
-
-        connection_ = std::make_shared<AMQP::TcpConnection>(*connectionHandler_.get(), AMQP::Address("amqp://localhost/"));
-
-        channel_ = std::make_shared<AMQP::TcpChannel>(connection_.get());
-
-        channel_->onError([this](const char *message) {
-            std::cout<<"(RMQ rx) Channel error: " << message;
-
-            if (exchangeName_.empty())
+    void Start() 
+    {
+        while(true)
+        {    
+            try 
             {
-                std::cout<<"(RMQ rx) Details: exchange_name is empty!";
+                connectionHandler_ = std::make_shared<ConnHandler>();
+
+                connection_ = std::make_shared<AMQP::TcpConnection>(*connectionHandler_.get(), AMQP::Address("amqp://localhost/"));
+
+                channel_ = std::make_shared<AMQP::TcpChannel>(connection_.get());
+
+                channel_->onError([this](const char *message) {
+                    std::cout<<"(RMQ rx) Channel error: " << message;
+
+                    if (exchangeName_.empty())
+                    {
+                        std::cout<<"(RMQ rx) Details: exchange_name is empty!";
+                    }
+                    // Stop the handler
+                    this->connectionHandler_->Stop();
+
+                });
+                channel_->declareExchange(exchangeName_, AMQP::fanout)
+                    .onSuccess([this]() {
+                        std::cout<<"(RMQ rx) Exchange '" << exchangeName_ << "' declared successfully.";
+                        }
+                    )		
+                    .onError([this](const char *message) {
+                        std::cout<<"(RMQ rx) Exchange '" << exchangeName_ << "' already exists. " << message;
+                        }
+                    );
+
+                channel_->declareQueue(queueName_, AMQP::durable)
+                    .onSuccess
+                    (
+                        [](const std::string &name,
+                        uint32_t messagecount,
+                        uint32_t consumercount) {
+                            std::cout<<"(RMQ rx) Created queue: " << name;
+                        }
+                    )
+                    .onError([](const char *message) {
+                        std::cout<<"(RMQ rx) Channel queue Error: " << message;
+                        }
+                    );
+
+                channel_->bindQueue(exchangeName_, queueName_, "")
+                    .onSuccess([]() {
+                        std::cout<<"(RMQ rx) Successfully bound queue to exchange.";
+                    })
+                    .onError([](const char *message) {
+                        std::cout<<"(RMQ rx) Error binding queue to exchange: " << message;
+                    });
+
+
+                channel_->consume(queueName_)
+                    .onReceived([this](const AMQP::Message& message, uint64_t tag, bool) {
+                        auto messageBody = std::string(message.body(), message.bodySize());
+                        std::cout<< "Message consumed: "<<messageBody;
+                        channel_->ack(tag);
+                    })
+                    .onError([](const char* message) {
+                        std::cerr << "Received an error while consuming message: " << message << std::endl;
+                    });
+                    
+            } 
+            catch (const std::exception& e) 
+            {
+                std::cerr << "Failed to start RabbitMQ receiver: " << e.what() << std::endl;
             }
-            // Stop the handler
-            this->connectionHandler_->Stop();
 
-        });
-        channel_->declareExchange(exchangeName_, AMQP::fanout)
-                .onSuccess([this]() {
-                    std::cout<<"(RMQ rx) Exchange '" << exchangeName_ << "' declared successfully.";
-                    }
-                )		
-                .onError([this](const char *message) {
-                    std::cout<<"(RMQ rx) Exchange '" << exchangeName_ << "' already exists. " << message;
-                    }
-                );
+            connectionHandler_->Start();
 
-        channel_->declareQueue(queueName_, AMQP::durable)
-                .onSuccess
-                (
-                    [](const std::string &name,
-                    uint32_t messagecount,
-                    uint32_t consumercount) {
-                        std::cout<<"(RMQ rx) Created queue: " << name;
-                    }
-                )
-                .onError([](const char *message) {
-                    std::cout<<"(RMQ rx) Channel queue Error: " << message;
-                    }
-                );
-
-        channel_->bindQueue(exchangeName_, queueName_, "")
-            .onSuccess([]() {
-                std::cout<<"(RMQ rx) Successfully bound queue to exchange.";
-            })
-            .onError([](const char *message) {
-                std::cout<<"(RMQ rx) Error binding queue to exchange: " << message;
-            });
-
-
-        channel_->consume(queueName_)
-            .onReceived([this](const AMQP::Message& message, uint64_t tag, bool) {
-                auto messageBody = std::string(message.body(), message.bodySize());
-                //std::cout<< "Message consumed: "<<messageBody;
-                channel_->ack(tag);
-            })
-            .onError([](const char* message) {
-                std::cerr << "Received an error while consuming message: " << message << std::endl;
-            });
-                
-        } 
-        catch (const std::exception& e) 
-        {
-            std::cerr << "Failed to start RabbitMQ receiver: " << e.what() << std::endl;
+            // Tidy up after
+            this->connection_->close();
+            this->channel_.reset();
+            this->connection_.reset();
+            this->connectionHandler_.reset();
         }
-
-        connectionHandler_->Start();
-
-        // Tidy up after
-        this->connection_->close();
-        this->channel_.reset();
-        this->connection_.reset();
-        this->connectionHandler_.reset();
     }
-    
-  }
 
-  std::string exchangeName_;
-  std::string queueName_;
-  std::string routingKey_;
-  std::shared_ptr<AMQP::TcpConnection> connection_;
-  std::shared_ptr<AMQP::TcpChannel> channel_;
-  std::shared_ptr<ConnHandler> connectionHandler_;
-
-   std::thread processingThread;
+    std::string exchangeName_;
+    std::string queueName_;
+    std::string routingKey_;
+    std::shared_ptr<AMQP::TcpConnection> connection_;
+    std::shared_ptr<AMQP::TcpChannel> channel_;
+    std::shared_ptr<ConnHandler> connectionHandler_;
+    std::thread processingThread;
 };
 
-int main() {
-  std::string exchangeName = "example_exchange";
-  std::string queueName = "example_queue";
-  std::string routingKey = "";
-
+int main() 
+{
+    std::string exchangeName = "example_exchange";
+    std::string queueName = "example_queue";
+    std::string routingKey = "";
 
     std::thread receiverThread([&]() 
     {
         RabbitMQReceiver receiver(exchangeName, queueName, routingKey);
 
         // Wait for a message to be received
-        // std::cout << "Waiting for message..." << std::endl;
         while (true) 
         {
-            //std::cout << "Receiving messages...";
-
             std::this_thread::sleep_for(std::chrono::microseconds(100));
         }
     });
@@ -318,9 +381,8 @@ int main() {
         RabbitMQTransmitter transmitter(exchangeName, queueName);
         while(true)
         {
-            //std::cout <<"Send msg";
             transmitter.SendMessage("Hello, world!");
-            std::this_thread::sleep_for(std::chrono::microseconds(100));
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
         }
     });
 
